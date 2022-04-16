0b07f3af01327cd7e4922c23b86f2786498ef913
Gabrielmkmk <gabriel.marianok@unesc.net>
2022-04-16 00:46:08 -0300

    Muda status da mesa ao cancelar reserva

--- app/src/main/java/alisson/zanoni/nostroristorante/CancelarReservaActivity.java
+++ app/src/main/java/alisson/zanoni/nostroristorante/CancelarReservaActivity.java
@@ -15,12 +15,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.util.List;
 
 import alisson.zanoni.nostroristorante.model.Reserva;
+import alisson.zanoni.nostroristorante.repository.MesaFireBaseRepository;
 import alisson.zanoni.nostroristorante.repository.ReservaFireBaseRepository;
 
 public class CancelarReservaActivity extends AppCompatActivity {
 
     private ListView listaReservasCancelar;
     ReservaFireBaseRepository reservaFireBaseRepository;
+    MesaFireBaseRepository mesaFireBaseRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,7 @@ public class CancelarReservaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cancelar_reserva);
 
         listaReservasCancelar = findViewById(R.id.listaReservasCancelar);
+        mesaFireBaseRepository = new MesaFireBaseRepository();
 
         reservaFireBaseRepository = new ReservaFireBaseRepository();
         String idUsuario =  FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -46,6 +49,7 @@ public class CancelarReservaActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Reserva reservaEscolhida = (Reserva) listaReservasCancelar.getItemAtPosition(i);
                 reservaFireBaseRepository.deletarReserva(idUsuario, reservaEscolhida.getNumeracaoMesa());
+                mesaFireBaseRepository.updateStatus(reservaEscolhida.getNumeracaoMesa(), false);
                 Toast.makeText(CancelarReservaActivity.this, "Reserva deletada com sucesso!", Toast.LENGTH_SHORT).show();
                 Intent trocarTela = new Intent(CancelarReservaActivity.this, MenuReservaActivity.class);
                 startActivity(trocarTela);
