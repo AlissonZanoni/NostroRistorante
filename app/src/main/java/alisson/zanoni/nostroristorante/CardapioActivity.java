f545106b491071f58d31d154585c4ae692ca8767
aliss <alisson.zanonii@gmail.com>
2022-04-24 11:53:52 -0300

    Criando a tela de Cardapio

--- app/src/main/java/alisson/zanoni/nostroristorante/CardapioActivity.java
+++ app/src/main/java/alisson/zanoni/nostroristorante/CardapioActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class CardapioActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnEntrada, btnAcompanhamento, btnMassa, btnRisoto, btnCarne, btnDoce, btnBebida, btnVinho, btnCerveja;
+    LinearLayout btnEntrada, btnAcompanhamento, btnMassa, btnRisoto, btnCarne, btnDoce, btnBebida, btnVinho, btnCerveja;
     public static final int telaParams = 1;
     Bundle params;
 
